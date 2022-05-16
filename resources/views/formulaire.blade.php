57325bee970d30354a0967e029e68ed979f803dc
barbeprisc <barbepriscamam@gmail.com>
2022-05-16 13:56:44 +0000

    mes pages prisca

--- resources/views/formulaire.blade.php
+++ resources/views/formulaire.blade.php
@@ -7,49 +7,61 @@
 <link rel="stylesheet" href="{{ asset('style/bootstrap/css/bootstrap.css') }}">
 
 <body>
-    <h1 class="text-center">Formulaire des étudiants</h1>
-    <div class="container my-5">
-        <form method="post">
-            <div class="mb-3">
-                <label>Nom</label>
-                <input type="text" class="form-control" placeholder="Entrez votre nom" name="name" autocomplete="off">
+    <h1 style="text-align:center">Formulaire insertion</h1>
+
+    <div class="container">
+        <a href="liste">
+            <button class="btn btn-danger">liste des etudiants</button>
+        </a>
+        <br><br>
+        <form>
+
+            <div class="row">
+                <div class="form-group col-lg-6">
+                  <label for="nom">nom</label>
+                  <input type="texte" class="form-control" name="nom" placeholder="nom">
+                </div>
+                <div class="form-group col-lg-6">
+                  <label for="prenom">prenom</label>
+                  <input type="texte" class="form-control" name="prenom" placeholder="prenom">
+                </div>
+            </div>
+            <div class="row">
+                <div class="form-group col-lg-6">
+                  <label for="Age">Age</label>
+                  <input type="texte" class="form-control" name="nom" placeholder="Age">
+                </div>
+                <div class="form-group col-lg-6">
+                  <label for="Genre">Genre</label>
+                  <input type="texte" class="form-control" name="Genre" placeholder="Genre">
+                </div>
             </div>
 
-            <form method="post">
-                <div class="mb-3">
-                    <label>Prénom</label>
-                    <input type="text" class="form-control" placeholder="Entrez votre prénom" name="name">
+            <div class="row">
+
+                <div class="form-group col-lg-6">
+                    <label for="Adresse">Adresse</label>
+                    <input type="number" class="form-control" id="Adresse" placeholder="Adresse">
                 </div>
-                <form method="post">
-                    <div class="mb-3">
-                        <label>Genre</label>
-                        <input type="text" class="form-control" placeholder="Entrez votre genre" name="name">
-                    </div>
-                    <form method="post">
-                        <div class="mb-3">
-                            <label>Age</label>
-                            <input type="text" class="form-control" placeholder="Entrez votre âge" name="name">
-                        </div>
-                        <form method="post">
-                            <div class="mb-3">
-                                <label>Telephone</label>
-                                <input type="text" class="form-control" placeholder="Entrez votre telephone"
-                                    name="name">
-                            </div>
-                            <form method="post">
-                                <div class="mb-3">
-                                    <label>Adresse</label>
-                                    <input type="text" class="form-control" placeholder="Entrez votre adresse"
-                                        name="name">
-                                </div>
-                                <form method="post">
-                                    <div class="mb-3">
-                                        <label>Email</label>
-                                        <input type="text" class="form-control" placeholder="Entrez votre email" name="name">
-                                    </div>
-    </div>
-    <button type="submit" class="btn btn-primary">Submit</button>
-    </form>
+                <div class="form-group col-lg-6">
+                    <label for="inputEmail4">Telephone</label>
+                    <input type="Telephone" class="form-control" id="inputEmail4" placeholder="Telephone">
+                </div>
+
+            </div>
+            <br>
+            <div class="row">
+                <div class="col-lg-6">
+                    <input type="submit" class="btn btn-primary" value="valider">
+                </div>
+                <div class="col-lg-6">
+                    <button type="submit" class="btn btn-primary">annuler</button>
+                </div>
+
+
+            </div>
+
+          </form>
     </div>
 
 
--- resources/views/liste.blade.php
+++ resources/views/liste.blade.php
@@ -1,4 +1,79 @@
-@extends('layout')
-<h1>La liste des étudiants</h1>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
 
-@section()
+</head>
+<link rel="stylesheet" href="{{ asset('style/bootstrap/css/bootstrap.min.css') }}">
+<link rel="stylesheet" href="{{ asset('style/bootstrap/css/bootstrap.css') }}">
+
+<body>
+
+    <h2 style="text-align: center;">Listes des etudiants</h2>
+
+    <a href="formulaire">
+        <button class="btn btn-success" style="margin-left: 1000;">formulaire</button>
+    </a>
+    <br><br>
+
+    <div class="container">
+        <caption>Liste des etudiants</caption>
+        <table class="table">
+
+            <thead>
+              <tr>
+                <th scope="col">numero</th>
+                <th scope="col">nom</th>
+                <th scope="col">prenom</th>
+                <th scope="col">age</th>
+                <th scope="col">genre</th>
+                <th scope="col">adresse</th>
+                <th scope="col">telephone</th>
+                <th scope="col">email</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <th scope="row">1</th>
+                <td>Nikiema</td>
+                <td>Mariam</td>
+                <td>feminin</td>
+                <td>24</td>
+                <td>75 45 02 22</td>
+                <td>nikiema@gmail.com</td>
+              </tr>
+              <tr>
+                <th scope="row">2</th>
+                <td>Konate</td>
+                <td>Ulriche</td>
+                <td>masculin</td>
+                <td>30</td>
+                <td>70 55 20 20</td>
+                <td>konateu@gmail.com</td>
+              </tr>
+              <tr>
+                <th scope="row">3</th>
+                <td>Ngoma</td>
+                <td>Brice</td>
+                <td>masculin</td>
+                <td>32</td>
+                <td>65 45 32 51</td>
+                <td>ngomab@gmail.com</td>
+              </tr>
+              <tr>
+                <th scope="row">4</th>
+                <td>Mabiala</td>
+                <td>Blandine</td>
+                <td>feminin</td>
+                <td>25</td>
+                <td>76 02 00 51</td>
+                <td>mabialab@gmail.com</td>
+              </tr>
+            </tbody>
+          </table>
+    </div>
+</body>
+</html>
