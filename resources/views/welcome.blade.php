06850ba5216aa6817da6a091a0f0e98c5749a265
email <robinsondepot@gmail.com>
2022-05-16 11:47:34 +0000

    data

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -12,10 +12,10 @@
     <link rel="stylesheet" href="{{ asset('style/bootstrap/css/bootstrap.css') }}">
 
 <body>
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <div class="container-fluid">
+    <nav class="navbar navbar-expand-lg navbar-light bg-light" >
+        <div class="container-fluid " style="background-color: black;">
             <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="/">ACCUEIL</a>
+                <a class="nav-link active" href="/">ACCUEIL</a>
             </li>
             <li class="nav-item">
                 <a class="nav-link active" aria-current="page" href="formulaire">FORMULAIRE</a>
@@ -30,6 +30,7 @@
             </form>
         </div>
     </nav>
+    <img src="" alt="">
     @yield('content')
 </body>
 
